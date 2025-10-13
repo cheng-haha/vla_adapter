19000890cb32e56e155b89616e8b9357136caf9d
cheng-haha <3505242192@qq.com>
2025-10-13 10:16:07 +0000

    new

--- run_scripts/train/vla_adapter/libero/libero_10_sim_fusion.sh
+++ run_scripts/train/vla_adapter/libero/libero_10_sim_fusion.sh
@@ -40,13 +40,14 @@ save_latest_checkpoint_only=False
 merge_lora_during_training=True
 use_pro_version=True
 only_simple_action_head=True
+action_pooling_type=linear_fusion
 # Wandb settings
 wandb_entity=chenghaha
 wandb_project=vla_adapter
 
 # Generate timestamp and run ID
 current_time=$(date +"%Y%m%d_%H%M%S")
-run_id_note="sim-attention"
+run_id_note="sim-linear_fusion"
 
 # Build MODE string with important configuration variables (excluding those already in run_id)
 # run_id already includes: config_file_path, dataset_name, batch_size*grad_accumulation_steps, learning_rate, lora_rank, image_aug
@@ -85,7 +86,8 @@ torchrun --standalone --nnodes 1 --nproc-per-node 4 vla-scripts/finetune.py \
   --wandb_entity "$wandb_entity" \
   --wandb_project "$wandb_project" \
   --run_id_note $run_id_note \
-  --only_simple_action_head $only_simple_action_head
+  --only_simple_action_head $only_simple_action_head \
+  --action_pooling_type $action_pooling_type
 
 echo "Training started with run ID: $run_id_note"
 echo "Output directory: $run_root_dir"
--- run_scripts/train/vla_adapter/libero/libero_10_sim_mean.sh
+++ run_scripts/train/vla_adapter/libero/libero_10_sim_mean.sh
@@ -24,7 +24,7 @@ batch_size=16
 grad_accumulation_steps=1
 learning_rate=2e-4
 max_steps=60005
-num_steps_before_decay=50000
+num_steps_before_decay=30000
 save_freq=10000
 
 # Model configuration
